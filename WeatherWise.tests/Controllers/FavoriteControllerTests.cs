4570b28876659107752542a411c81704a137385e
Luiz da Silva Moura <luiznetosilva@hotmail.com>
2024-10-04 16:38:30 -0300

    inplemented tests

---
+++ WeatherWise.tests/Controllers/FavoriteControllerTests.cs
@@ -0,0 +1,159 @@
+﻿using Moq;
+using WeatherWise_api.Controllers;
+using WeatherWise_api.Repository;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using WeatherWise_api.Modal;
+
+namespace FavoriteTests.Controllers;
+public class FavoriteControllerTests
+{
+    private readonly Mock<IFavoriteRepository> _favoriteRepositoryMock;
+    private readonly Mock<ILogger<WeatherForecastController>> _loggerMock;
+    private readonly FavoriteController _favoriteController;
+    
+    public FavoriteControllerTests()
+    {
+        _favoriteRepositoryMock = new Mock<IFavoriteRepository>();
+        _loggerMock = new Mock<ILogger<WeatherForecastController>>();
+        _favoriteController = new FavoriteController(_loggerMock.Object, _favoriteRepositoryMock.Object);
+    }
+
+    #region Get
+    /// <summary>
+    /// Vaidação para quando não é enviado valor para a requisição 
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task GetFavorite_ReturnsBadRequest_WhenEmailIsNull()
+    {
+        var result = await _favoriteController.GetFavorite(null);
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+    /// <summary>
+    /// Validação para quando eviado email e retorna countryID
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task GetFavorite_ReturnsOkResult_WithFavorites()
+    {
+        var email = "test@example.com";
+        var mockFavorites = new List<Favorite> { new Favorite { Email = email, CountryId = "xx" } };
+        _favoriteRepositoryMock.Setup(repo => repo.GetFavorites(email)).ReturnsAsync(mockFavorites);
+
+        var result = await _favoriteController.GetFavorite(email);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<List<Favorite>>(okResult.Value);
+        Assert.Single(returnValue);
+    }
+    /// <summary>
+    /// validação para quando da erro na requicição com o dynamo DB
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task GetFavorite_Returns500_WhenExceptionIsThrown()
+    {
+        var email = "test@example.com";
+        _favoriteRepositoryMock.Setup(repo => repo.GetFavorites(email)).ThrowsAsync(new Exception("Database error"));
+
+        var result = await _favoriteController.GetFavorite(email);
+
+        var statusCodeResult = Assert.IsType<ObjectResult>(result);
+        Assert.Equal(500, statusCodeResult.StatusCode);
+    }
+    /// <summary>
+    /// Validação para quando o email ou countryId não forem fornecidos.
+    /// </summary>
+    /// <returns></returns>
+    #endregion
+    
+    #region Post
+    [Fact]
+    public async Task AddFavorite_ReturnsBadRequest_WhenEmailOrCountryIdIsNull()
+    {
+        var result = await _favoriteController.AddFavorite(null, "BR");
+
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+
+    /// <summary>
+    /// Validação para quando um favorito é adicionado com sucesso.
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task AddFavorite_ReturnsOkResult_WhenFavoriteIsAdded()
+    {
+        var email = "test@example.com";
+        var countryId = "BR";
+
+        var result = await _favoriteController.AddFavorite(email, countryId);
+
+        Assert.IsType<OkResult>(result);
+    }
+
+    /// <summary>
+    /// Validação para quando há um erro na requisição ao DynamoDB.
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task AddFavorite_Returns500_WhenExceptionIsThrown()
+    {
+        var email = "test@example.com";
+        var countryId = "BR";
+        _favoriteRepositoryMock.Setup(repo => repo.AddFavorite(email, countryId)).ThrowsAsync(new Exception("Database error"));
+
+        var result = await _favoriteController.AddFavorite(email, countryId);
+
+        var statusCodeResult = Assert.IsType<ObjectResult>(result);
+        Assert.Equal(500, statusCodeResult.StatusCode);
+    }
+    #endregion
+    
+    #region Delete
+    /// <summary>
+    /// Validação para quando o email ou countryId não forem fornecidos.
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task DeleteFavorite_ReturnsBadRequest_WhenEmailOrCountryIdIsNull()
+    {
+        var result = await _favoriteController.DeleteFavorite(null, "BR");
+
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+
+    /// <summary>
+    /// Validação para quando um favorito é deletado com sucesso.
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task DeleteFavorite_ReturnsOkResult_WhenFavoriteIsDeleted()
+    {
+        var email = "test@example.com";
+        var countryId = "BR";
+
+        var result = await _favoriteController.DeleteFavorite(email, countryId);
+
+        Assert.IsType<OkResult>(result);
+    }
+
+    /// <summary>
+    /// Validação para quando há um erro na requisição ao DynamoDB.
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task DeleteFavorite_Returns500_WhenExceptionIsThrown()
+    {
+        var email = "test@example.com";
+        var countryId = "BR";
+        _favoriteRepositoryMock.Setup(repo => repo.DeleteFavorite(email, countryId)).ThrowsAsync(new Exception("Database error"));
+
+        var result = await _favoriteController.DeleteFavorite(email, countryId);
+
+        var statusCodeResult = Assert.IsType<ObjectResult>(result);
+        Assert.Equal(500, statusCodeResult.StatusCode);
+    }
+    #endregion
+
+}
---
+++ WeatherWise.tests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,78 @@
+﻿using Moq;
+using WeatherWise_api.Controllers;
+using WeatherWise_api.Services;
+using WeatherWise_api.Modal;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecast.tests.Controllers;
+public class WeatherForecastControllerTests
+{
+    private readonly Mock<IWeatherService> _weatherServiceMock;
+    private readonly Mock<ILogger<WeatherForecastController>> _loggerMock;
+    private readonly WeatherForecastController _weatherForecastController;
+
+    public WeatherForecastControllerTests()
+    {
+        _weatherServiceMock = new Mock<IWeatherService>();
+        _loggerMock = new Mock<ILogger<WeatherForecastController>>();
+        _weatherForecastController = new WeatherForecastController(_loggerMock.Object, _weatherServiceMock.Object);
+    }
+
+    /// <summary>
+    /// Validação para quando o país ou a capital não são fornecidos.
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task GetWeatherForecast_ReturnsBadRequest_WhenCountryOrCapitalIsNull()
+    {
+        var result = await _weatherForecastController.GetWeatherForecast(null, "Brasília");
+
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+
+    /// <summary>
+    /// Validação para quando a previsão do tempo é retornada com sucesso.
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task GetWeatherForecast_ReturnsOkResult_WhenWeatherInfoIsRetrieved()
+    {
+        var country = "Brasil";
+        var capital = "Brasília";
+        var weatherInfo = new WeatherInfo
+        {
+            Temperature = 30,
+            Description = "Clear sky"
+        };
+
+        _weatherServiceMock.Setup(service => service.GetWeatherByCountryAndCapital(country, capital))
+            .ReturnsAsync(weatherInfo);
+
+        var result = await _weatherForecastController.GetWeatherForecast(country, capital);
+
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnedWeather = Assert.IsType<WeatherInfo>(okResult.Value);
+        Assert.Equal(30, returnedWeather.Temperature);
+        Assert.Equal("Clear sky", returnedWeather.Description);
+    }
+
+    /// <summary>
+    /// Validação para quando ocorre uma exceção ao tentar obter as informações do tempo.
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task GetWeatherForecast_Returns500_WhenExceptionIsThrown()
+    {
+        var country = "Brasil";
+        var capital = "Brasília";
+
+        _weatherServiceMock.Setup(service => service.GetWeatherByCountryAndCapital(country, capital))
+            .ThrowsAsync(new Exception("API error"));
+
+        var result = await _weatherForecastController.GetWeatherForecast(country, capital);
+
+        var statusCodeResult = Assert.IsType<ObjectResult>(result);
+        Assert.Equal(500, statusCodeResult.StatusCode);
+    }
+}
